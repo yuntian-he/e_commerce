ed88faa53f7ea09294360c9b57a3f066384bf25a
matthew_he <hytian994@mail.com>
2023-05-18 23:05:52 -0400

    Update Backend

--- src/main/java/com/luv2code/ecommerce/dao/OrderRepository.java
+++ src/main/java/com/luv2code/ecommerce/dao/OrderRepository.java
@@ -1,6 +1,6 @@
 package com.luv2code.ecommerce.dao;
 
-import com.luv2code.ecommerce.entity.Product;
+import com.luv2code.ecommerce.entity.Order;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,9 +8,8 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ProductRespository extends JpaRepository<Product, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long> {
 
-    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
+    Page<Order> findByCustomerEmailOrderByDateCreatedDesc(@Param("email") String email, Pageable pageable);
 
-    Page<Product> findByNameContaining(@Param("name") String name, Pageable page);
 }
